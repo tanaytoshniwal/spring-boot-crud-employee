7556f7496c591c58e3cd995fd2d055742fe36e4f
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2023-10-03 21:27:46 +0530

    customizing spring-data-rest config

--- src/main/java/com/ck/spb/crudemployee/dao/EmployeeRepository.java
+++ src/main/java/com/ck/spb/crudemployee/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.ck.spb.crudemployee.dao;
 
 import com.ck.spb.crudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// customizing the endpoint from employees to members
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
